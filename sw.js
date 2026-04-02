ea9f7c60a4ff0681af34667c8e8889e49ec299d7
michelenapione-sketch <michele.napione@gmail.com>
2026-04-02 13:56:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', e => {
  e.waitUntil(
-   caches.open('akc-cache-v1').then(cache => {
+   caches.open('akc-cache-v2').then(cache => {
      return cache.addAll(['index.html']);
    })
  );
